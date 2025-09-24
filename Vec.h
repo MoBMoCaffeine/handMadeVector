cffe29604f27399fd288cb23213264bf67e05ea8
MoBMoCaffeine <bakrm1921@gmail.com>
2025-09-24 20:40:09 +0300

    new file added (header file is ready to use)

---
+++ Vec.h
@@ -0,0 +1,399 @@
+#pragma once
+
+#include <stdexcept>
+#include <utility>
+#include <initializer_list>
+#include <iostream>
+#include <iterator>
+
+template <typename T>
+class Vec
+{
+private:
+    T *data;         
+    size_t capacity; 
+    size_t length;   
+
+    void resize_capacity(size_t new_capacity)
+    {
+        T *new_data = new T[new_capacity];
+        for (size_t i = 0; i < length; ++i)
+        {
+            new_data[i] = std::move(data[i]);
+        }
+        delete[] data;
+        data = new_data;
+        capacity = new_capacity;
+    }
+
+    void merge_sort(int left, int right)
+    {
+        if (left >= right) return;
+        int mid = left + (right - left) / 2;
+        merge_sort(left, mid);
+        merge_sort(mid + 1, right);
+        merge_arrays(left, mid, right);
+    }
+
+    void merge_arrays(int left, int mid, int right)
+    {
+        int n1 = mid - left + 1, n2 = right - mid;
+        T *leftArr = new T[n1];
+        T *rightArr = new T[n2];
+
+        for (int i = 0; i < n1; ++i)
+            leftArr[i] = std::move(data[left + i]);
+        for (int j = 0; j < n2; ++j)
+            rightArr[j] = std::move(data[mid + 1 + j]);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2)
+        {
+            data[k++] = (leftArr[i] <= rightArr[j]) ? std::move(leftArr[i++]) : std::move(rightArr[j++]);
+        }
+        while (i < n1) data[k++] = std::move(leftArr[i++]);
+        while (j < n2) data[k++] = std::move(rightArr[j++]);
+
+        delete[] leftArr;
+        delete[] rightArr;
+    }
+
+public:
+    Vec() : data(nullptr), capacity(0), length(0) {}
+    Vec(size_t size, const T &init_value) : data(nullptr), capacity(0), length(0)
+    {
+        if (size > 0)
+        {
+            resize_capacity(size);
+            length = size;
+            for (size_t i = 0; i < size; ++i)
+                data[i] = init_value;
+        }
+    }
+
+    Vec(std::initializer_list<T> init_list) : data(nullptr), capacity(0), length(0)
+    {
+        size_t auto_size = init_list.size();
+        resize_capacity(auto_size);
+        length = auto_size;
+        size_t i = 0;
+        for (const T &item : init_list)
+            data[i++] = item;
+    }
+
+    ~Vec() { delete[] data; }
+
+    Vec(const Vec<T> &other) : data(nullptr), capacity(0), length(0)
+    {
+        if (other.length > 0)
+        {
+            resize_capacity(other.length);
+            length = other.length;
+            for (size_t i = 0; i < length; ++i)
+                data[i] = other.data[i];
+        }
+    }
+
+    Vec<T> &operator=(const Vec<T> &other)
+    {
+        if (this != &other)
+        {
+            Vec<T> temp(other);
+            swap(temp);
+        }
+        return *this;
+    }
+
+    Vec(Vec<T> &&other) noexcept : data(nullptr), capacity(0), length(0) { swap(other); }
+
+    Vec<T> &operator=(Vec<T> &&other) noexcept
+    {
+        if (this != &other)
+        {
+            delete[] data;
+            data = nullptr;
+            capacity = length = 0;
+            swap(other);
+        }
+        return *this;
+    }
+
+    void push_back(const T &value)
+    {
+        if (length == capacity)
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : capacity * 2;
+            resize_capacity(new_capacity);
+        }
+        data[length++] = value;
+    }
+
+    template <typename... Args>
+    void emplace_back(Args&&... args)
+    {
+        if (length == capacity)
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : capacity * 2;
+            resize_capacity(new_capacity);
+        }
+        data[length++] = T(std::forward<Args>(args)...);
+    }
+
+    void pop_back()
+    {
+        if (length == 0) throw std::out_of_range("pop_back() called on empty Vec");
+        length--;
+    }
+
+    void resize(size_t new_size)
+    {
+        if (new_size > capacity)
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : capacity;
+            while (new_capacity < new_size)
+                new_capacity *= 2;
+            resize_capacity(new_capacity);
+        }
+        if (new_size > length)
+        {
+            for (size_t i = length; i < new_size; ++i)
+                data[i] = T();
+        }
+        length = new_size;
+    }
+
+    void reserve(size_t new_capacity)
+    {
+        if (new_capacity > capacity)
+        {
+            resize_capacity(new_capacity);
+        }
+    }
+
+    void shrink_to_fit()
+    {
+        if (length < capacity)
+        {
+            resize_capacity(length);
+        }
+    }
+
+    T &front()
+    {
+        if (length == 0) throw std::out_of_range("front() called on empty Vec");
+        return data[0];
+    }
+
+    T &back()
+    {
+        if (length == 0) throw std::out_of_range("back() called on empty Vec");
+        return data[length - 1];
+    }
+
+    void insert(size_t index, const T &value)
+    {
+        if (index > length) throw std::out_of_range("insert() index out of range");
+        if (length == capacity)
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : capacity * 2;
+            resize_capacity(new_capacity);
+        }
+        for (size_t i = length; i > index; --i)
+            data[i] = std::move(data[i - 1]);
+        data[index] = value;
+        ++length;
+    }
+
+    template <typename... Args>
+    void emplace(size_t index, Args&&... args)
+    {
+        if (index > length) throw std::out_of_range("emplace() index out of range");
+        if (length == capacity)
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : capacity * 2;
+            resize_capacity(new_capacity);
+        }
+        for (size_t i = length; i > index; --i)
+            data[i] = std::move(data[i - 1]);
+        data[index] = T(std::forward<Args>(args)...);
+        ++length;
+    }
+
+    void merge(const Vec<T> &other)
+    {
+        if (length + other.length > capacity)
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : capacity;
+            while (new_capacity < length + other.length)
+                new_capacity *= 2;
+            resize_capacity(new_capacity);
+        }
+        for (size_t i = 0; i < other.length; ++i)
+        {
+            data[length + i] = other.data[i];
+        }
+        length += other.length;
+        merge_sort(0, length - 1);
+    }
+
+    void clear() { length = 0; }
+
+    T &operator[](size_t index) const
+    {
+        if (index >= length) throw std::out_of_range("Index out of range");
+        return data[index];
+    }
+
+    void sort()
+    {
+        if (length > 1) merge_sort(0, length - 1);
+    }
+
+    void swap(Vec<T> &other)
+    {
+        std::swap(data, other.data);
+        std::swap(length, other.length);
+        std::swap(capacity, other.capacity);
+    }
+
+    friend void swap(Vec<T> &a, Vec<T> &b)
+    {
+        using std::swap;
+        swap(a.data, b.data);
+        swap(a.length, b.length);
+        swap(a.capacity, b.capacity);
+    }
+
+    // Assign overloads
+    void assign(size_t n, const T &value)
+    {
+        if (n > capacity) resize_capacity(n);
+        for (size_t i = 0; i < n; ++i)
+            data[i] = value;
+        length = n;
+    }
+
+    void assign(std::initializer_list<T> ilist)
+    {
+        if (ilist.size() > capacity) resize_capacity(ilist.size());
+        size_t i = 0;
+        for (const T &val : ilist)
+            data[i++] = val;
+        length = ilist.size();
+    }
+
+    template <typename InputIt>
+    void assign(InputIt first, InputIt last)
+    {
+        size_t new_size = std::distance(first, last);
+        if (new_size > capacity) resize_capacity(new_size);
+        size_t i = 0;
+        for (auto it = first; it != last; ++it)
+            data[i++] = *it;
+        length = new_size;
+    }
+
+    // Erase
+    void erase(size_t index)
+    {
+        if (index >= length) throw std::out_of_range("erase() index out of range");
+        for (size_t i = index; i < length - 1; ++i)
+            data[i] = std::move(data[i + 1]);
+        --length;
+    }
+
+    void erase(size_t first, size_t last)
+    {
+        if (first >= length || last > length || first >= last)
+            throw std::out_of_range("erase() range invalid");
+        size_t range_size = last - first;
+        for (size_t i = first; i + range_size < length; ++i)
+            data[i] = std::move(data[i + range_size]);
+        length -= range_size;
+    }
+
+    // Comparison operators
+    bool operator==(const Vec<T> &other) const
+    {
+        if (length != other.length) return false;
+        for (size_t i = 0; i < length; ++i)
+            if (!(data[i] == other.data[i])) return false;
+        return true;
+    }
+
+    bool operator!=(const Vec<T> &other) const { return !(*this == other); }
+
+    bool operator<(const Vec<T> &other) const
+    {
+        size_t min_len = (length < other.length) ? length : other.length;
+        for (size_t i = 0; i < min_len; ++i)
+        {
+            if (data[i] < other.data[i]) return true;
+            if (other.data[i] < data[i]) return false;
+        }
+        return length < other.length;
+    }
+
+    bool operator>(const Vec<T> &other) const { return other < *this; }
+    bool operator<=(const Vec<T> &other) const { return !(other < *this); }
+    bool operator>=(const Vec<T> &other) const { return !(*this < other); }
+
+    // Concatenation
+    Vec<T> &operator+=(const Vec<T> &other)
+    {
+        if (length + other.length > capacity)
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : capacity;
+            while (new_capacity < length + other.length)
+                new_capacity *= 2;
+            resize_capacity(new_capacity);
+        }
+        for (size_t i = 0; i < other.length; ++i)
+            data[length + i] = other.data[i];
+        length += other.length;
+        return *this;
+    }
+
+    Vec<T> operator+(const Vec<T> &other) const
+    {
+        Vec<T> result(*this);
+        result += other;
+        return result;
+    }
+
+    // Iterators
+    T *begin() { return data; }
+    T *end() { return data + length; }
+    const T *begin() const { return data; }
+    const T *end() const { return data + length; }
+
+    // Capacity
+    size_t size() const { return length; }
+    size_t get_capacity() const { return capacity; }
+    bool empty() const { return length == 0; }
+
+    // Stream operators
+    friend std::ostream &operator<<(std::ostream &os, const Vec<T> &vec)
+    {
+        os << "[";
+        for (size_t i = 0; i < vec.length; ++i)
+        {
+            os << vec.data[i];
+            if (i + 1 < vec.length) os << ", ";
+        }
+        os << "]";
+        return os;
+    }
+
+    friend std::istream &operator>>(std::istream &is, Vec<T> &vec)
+    {
+        size_t n;
+        is >> n;
+        vec.resize(n);
+        for (size_t i = 0; i < n; ++i)
+        {
+            is >> vec.data[i];
+        }
+        return is;
+    }
+};
